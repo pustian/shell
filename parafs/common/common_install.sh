10fd7edfecd97eef6132cceb4f185d0888e3187e
pustian <pustian@msn.com>
2018-08-07 00:12:59 +0800

    yarn_mem config

--- parafs/common/common_install.sh
+++ parafs/common/common_install.sh
@@ -72,22 +72,22 @@ function update_bashrc() {
     return $?
 }
 
-###### 远程修改sed文件
-function update_sed_script() {
-    local local_user=$1
-    local authorize_ip=$2
-    local authorize_user=$3
-    local sed_script_file=$4
-    local sed_script=$5
-
-    sed_script="\<value\>hello world\</value\> \<!-- 田--\>"
- 
-    ### 修改sed文件 
-    local temp_file="/tmp/parafs_update_sed_script$authorize_ip"
-    local remote_command="echo $sed_script |sudo tee $sed_script_file"   
-    sudo su - $local_user -c "ssh '$authorize_user@$authorize_ip' '$remote_command'" >>$temp_file
-    return $?
-}
+####### 远程修改sed文件
+#function update_sed_script() {
+#    local local_user=$1
+#    local authorize_ip=$2
+#    local authorize_user=$3
+#    local sed_script_file=$4
+#    local sed_script=$5
+#
+#    sed_script="\<value\>hello world\</value\> \<!-- 田--\>"
+# 
+#    ### 修改sed文件 
+#    local temp_file="/tmp/parafs_update_sed_script$authorize_ip"
+#    local remote_command="echo $sed_script |sudo tee $sed_script_file"   
+#    sudo su - $local_user -c "ssh '$authorize_user@$authorize_ip' '$remote_command'" >>$temp_file
+#    return $?
+#}
 
 function update_hadoop_yarn_ip() {
     local local_user=$1
@@ -97,36 +97,81 @@ function update_hadoop_yarn_ip() {
     local sed_script_file=$5
     local main_ip=$6
 
+    local temp_file="/tmp/parafs_update_yarn_ip$authorize_ip"
     ### 1, 远程获取需要更新的行数，
-    local property_label="name"
-    local property_label_value="yarn.resourcemanager.hostname"
-    local remote_property_line="grep -n $property_label_value $filename |grep $property_label_value "
-    local remote_property_line_result=`sudo su - $local_user -c "ssh $authorize_user@$authorize_ip '$remote_property_line' "` 
-    if [ -z "$remote_property_line" ]; then 
+    local yarn_master_label="name"
+    local yarn_master_label_value="yarn.resourcemanager.hostname"
+    local remote_yarn_master_line="grep -n $yarn_master_label_value $filename |grep $yarn_master_label_value "
+    local remote_yarn_master_line_result=`sudo su - $local_user -c "ssh $authorize_user@$authorize_ip '$remote_yarn_master_line' "` 
+    if [ -z "$remote_yarn_master_line_result" ]; then 
         echo "pls check $filename at $authorize_ip"
     fi
-#    echo $remote_property_line_result
+    echo $remote_yarn_master_line_result >$temp_file
 
     ### 2, 在本地生成sed_script 然后复制到远端脚本所在地
-    local line_num=`echo "$remote_property_line"` | awk -F ':' '{print $1}'
-    local property_value="\<value\>${main_ip}\</value\>  \<!-- yarn主节点 --\>"
-    local sed_script="$(($line_num+1)),$(($line_num+1))c $property_value "   
-    echo $sed_script |sudo tee $sed_script_file >/dev/null
-    sudo su - $local_user -c "scp '$sed_script_file' '$authorize_user@$authorize_ip:$sed_script_file'"
+    local line_num=` echo "$remote_yarn_master_line_result" | awk -F ':' '{print $1}'`
+    local yarn_master_value="\<value\>${main_ip}\</value\>  \<!-- yarn主节点 --\>"
+    local sed_script="$(($line_num+1)),$(($line_num+1))c $yarn_master_value "   
+    echo $sed_script |sudo tee $sed_script_file >>$temp_file
+    sudo su - $local_user -c "scp '$sed_script_file' '$authorize_user@$authorize_ip:$sed_script_file'" >>$temp_file   
     
     ### 3, 远程执行sed脚本
     local remote_exec_sed_script="sed -i -f $sed_script_file $filename"
-    sudo su - $local_user -c "ssh '$authorize_user@$authorize_ip' '$remote_exec_sed_script'"
+    sudo su - $local_user -c "ssh '$authorize_user@$authorize_ip' '$remote_exec_sed_script'" >>$temp_file
     return $?
 }
 
-function update_hadoop_mem() {
+function update_hadoop_yarn_mem() {
     local local_user=$1
     local authorize_ip=$2
     local authorize_user=$3
+    local filename=$4
+    local sed_script_file=$5
 
-    local temp_file="/tmp/parafs_update_hadoop_mem$authorize_ip"
-    echo "do update_hadoop_mem at $authorize_ip "
+    local temp_file="/tmp/parafs_update_yarn_mem$authorize_ip"
+    ### 1 远程获取内存
+    local remote_mem_kb="grep MemTotal /proc/meminfo"
+    local remote_mem_kb_result=`sudo su - $local_user -c "ssh '$authorize_user@$authorize_ip' '$remote_mem_kb'" `
+    local mem_kb=`echo $remote_mem_kb_result | awk '{print $2}' `
+    # local mem_mb_2=$(($mem_kb/512))  # XXX/1024*2
+
+    ### 2,远程获取 总内存需要更新的行数，
+    local memory_label="name"
+    local memory_label_value="yarn.nodemanager.resource.memory-mb"
+    local remote_memory_line="grep -n $memory_label_value $filename |grep $memory_label_value "
+    local remote_memory_line_result=`sudo su - $local_user -c "ssh $authorize_user@$authorize_ip '$remote_memory_line' "` 
+    if [ -z "$remote_memory_line" ]; then 
+        echo "pls check $filename at $authorize_ip"
+    fi
+    echo $remote_memory_line_result >$temp_file
+
+    ### 3, 在本地生成sed_script 然后复制到远端脚本所在地
+    local line_num=`echo "$remote_memory_line_result" | awk -F ':' '{print $1}'`
+    local memory_value="\<value\>$(($mem_kb/512))\</value\>  \<!-- yarn使用总内存 --\>"
+    local sed_script="$(($line_num+1)),$(($line_num+1))c $memory_value "   
+    echo $sed_script |sudo tee $sed_script_file >>$temp_file
+    sudo su - $local_user -c "scp '$sed_script_file' '$authorize_user@$authorize_ip:$sed_script_file'" >>$temp_file   
+    
+    ### 4,远程获取 单个scheduler 内存 需要更新的行数，
+    local alloc_mem_label="name"
+    local alloc_mem_label_value="yarn.scheduler.maximum-allocation-mb"
+    local remote_alloc_mem_line="grep -n $alloc_mem_label_value $filename |grep $alloc_mem_label_value "
+    local remote_alloc_mem_line_result=`sudo su - $local_user -c "ssh $authorize_user@$authorize_ip '$remote_alloc_mem_line' "` 
+    if [ -z "$remote_alloc_mem_line" ]; then 
+        echo "pls check $filename at $authorize_ip"
+    fi
+    echo $remote_alloc_mem_line_result >$temp_file
+    ### 5, 在本地生成sed_script 然后复制到远端脚本所在地
+    local line_num=`echo "$remote_alloc_mem_line_result" | awk -F ':' '{print $1}'`
+    local alloc_mem_value="\<value\>$(($mem_kb/1024))\</value\>  \<!-- 单个进程最大占用内存 --\>"
+    local sed_script="$(($line_num+1)),$(($line_num+1))c $alloc_mem_value "   
+    echo $sed_script |sudo tee -a $sed_script_file >>$temp_file ## 此处为追加
+    sudo su - $local_user -c "scp '$sed_script_file' '$authorize_user@$authorize_ip:$sed_script_file'" >>$temp_file   
+
+    ### 3, 远程执行sed脚本
+    local remote_exec_sed_script="sed -i -f $sed_script_file $filename"
+    sudo su - $local_user -c "ssh '$authorize_user@$authorize_ip' '$remote_exec_sed_script'" >>$temp_file
+    return $?
 }
 function update_hadoop_scheduler_mem() {
 echo $?
@@ -186,6 +231,7 @@ COMMON_INSTALL_BASH_NAME=common_install.sh
 # echo $?
 # update_bashrc parauser 192.168.138.72 parauser /home/parauser /opt/wotung/parafs-install/conf/bashrc
 # update_sed_script parauser 192.168.138.70 parauser  /opt/wotung/parafs-install/conf/sed_script/hadoop/hadoop_yarn
-update_hadoop_yarn_ip parauser 192.168.138.71 parauser /opt/wotung/hadoop-parafs/hadoop-2.7.3/etc/hadoop/yarn-site.xml /opt/wotung/parafs-install/conf/sed_script/hadoop/hadoop_yarn 192.168.1.299
+ update_hadoop_yarn_ip parauser 192.168.138.71 parauser /opt/wotung/hadoop-parafs/hadoop-2.7.3/etc/hadoop/yarn-site.xml /opt/wotung/parafs-install/conf/sed_script/hadoop/hadoop_yarn_ip 192.168.1.299
+ update_hadoop_yarn_mem parauser 192.168.138.71 parauser /opt/wotung/hadoop-parafs/hadoop-2.7.3/etc/hadoop/yarn-site.xml /opt/wotung/parafs-install/conf/sed_script/hadoop/hadoop_yarn_mem
 echo $?
 ###++++++++++++++++++++++++      test end         ++++++++++++++++++++++++++###
